4177bacb7c185e26b4c6dfb43d05549c76d8caa0
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2022-11-07 00:42:56 -0800

    feet: added post features

---
+++ src/features/post/controllers/update-post.ts
@@ -0,0 +1,94 @@
+import { UploadApiResponse } from 'cloudinary';
+import { IPostDocument } from '@post/interfaces/post.interface';
+import { postSchema, postWithImageSchema } from '@post/schemes/post.schemes';
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { PostCache } from '@service/redis/post.cache';
+import { socketIOPostObject } from '@socket/post';
+import { postQueue } from '@service/queues/post.queue';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import { BadRequestError } from '@global/helpers/error-handler';
+
+const postCache: PostCache = new PostCache();
+
+export class Update {
+  @joiValidation(postSchema)
+  public async post(req: Request, res: Response): Promise<void> {
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+    const { postId } = req.params;
+    const updatedPost: IPostDocument = {
+      post,
+      bgColor,
+      privacy,
+      feelings,
+      gifUrl,
+      profilePicture,
+      imgId,
+      imgVersion
+    } as IPostDocument;
+
+    const postUpdated = await postCache.updatePostInCache(postId, updatedPost);
+    socketIOPostObject.emit('update post', postUpdated, 'posts');
+    postQueue.addPostJob('updatePostInDB', { key: postId, value: postUpdated });
+    res.status(HTTP_STATUS.OK).json({ message: 'Post updated successfully' });
+  }
+
+  @joiValidation(postWithImageSchema)
+  public async postWithImage(req: Request, res: Response): Promise<void> {
+    const { imgId, imgVersion } = req.body;
+    if (imgId && imgVersion) {
+      Update.prototype.updatPostWithImage(req);
+    } else {
+      const result: UploadApiResponse = await Update.prototype.addImageToExistingPost(req);
+      if (!result.public_id) {
+        throw new BadRequestError(result.message);
+      }
+    }
+    res.status(HTTP_STATUS.OK).json({ message: 'Post with image updated successfully' });
+  }
+
+  private async updatPostWithImage(req: Request): Promise<void> {
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+    const { postId } = req.params;
+    const updatedPost: IPostDocument = {
+      post,
+      bgColor,
+      privacy,
+      feelings,
+      gifUrl,
+      profilePicture,
+      imgId,
+      imgVersion
+    } as IPostDocument;
+
+    const postUpdated = await postCache.updatePostInCache(postId, updatedPost);
+    socketIOPostObject.emit('update post', postUpdated, 'posts');
+    postQueue.addPostJob('updatePostInDB', { key: postId, value: postUpdated });
+  }
+
+  private async addImageToExistingPost(req: Request): Promise<UploadApiResponse> {
+    const { post, bgColor, feelings, privacy, gifUrl, profilePicture, image } = req.body;
+    const { postId } = req.params;
+    const result: UploadApiResponse = (await uploads(image)) as UploadApiResponse;
+    if (!result?.public_id) {
+      return result;
+    }
+    const updatedPost: IPostDocument = {
+      post,
+      bgColor,
+      privacy,
+      feelings,
+      gifUrl,
+      profilePicture,
+      imgId: result.public_id,
+      imgVersion: result.version.toString()
+    } as IPostDocument;
+
+    const postUpdated = await postCache.updatePostInCache(postId, updatedPost);
+    socketIOPostObject.emit('update post', postUpdated, 'posts');
+    postQueue.addPostJob('updatePostInDB', { key: postId, value: postUpdated });
+    //call image queue to add iamge to mongodb database
+    return result;
+  }
+}
